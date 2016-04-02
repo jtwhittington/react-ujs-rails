be4664f439bb3694c38cced039b6fb06c514d597
Jon Stokes <jon@jonstokes.com>
2016-04-01 21:05:26 -0500

    First stab at porting this over

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = require('./lib/mount');
+module.exports = require('./lib/turbolinks');
+module.exports = require('./lib/turbolinks_classic');
+module.exports = require('./lib/turbolinks_classic_deprecated');
+module.exports = require('./lib/native');
+module.exports = require('./lib/event_setup');
